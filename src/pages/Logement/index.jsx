adc42f4c3fbf79922e782af224123d5e55bb3d64
Berkpigh <pierre.berkovits@netplus.ch>
2023-10-19 16:54:13 +0200

    LogementId

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,7 +1,17 @@
 import { useParams } from 'react-router-dom'
+import locationList from '../../data/vingtannonces.json'
 
 function Logement() {
   const { logId } = useParams()
-  return <div>{logId}</div>
+  var lInd = 0
+  function getLog() {
+    for (lInd = 0; lInd < locationList.length; lInd++) {
+      console.log(locationList[lInd].id, { logId })
+      if (locationList[lInd].id === { logId }.logId) {
+        return lInd
+      }
+    }
+  }
+  return <div>{locationList[getLog()].description}</div>
 }
 export default Logement
