53b04e0a1dc27795ad68598cf6806c3b975e4bcb
Berkpigh <pierre.berkovits@netplus.ch>
2023-11-04 11:50:04 +0100

    desktop +++

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,18 +1,25 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
+import Footer from '../../components/Footer'
 
 function Error() {
   return (
-    <div className="error">
-      <div className="error-frame">
-        <p className="error-frame__p1">404</p>
-        <p className="error-frame__p2">
-          Oups! La page que vous demandez n'existe pas.
-        </p>
-        <Link to="/" className="error-frame__back">
-          Retourner sur la page d’accueil
-        </Link>
+    <React.Fragment>
+      <div>
+        <div className="error">
+          <div className="error-frame">
+            <p className="error-frame__p1">404</p>
+            <p className="error-frame__p2">
+              Oups! La page que vous demandez n'existe pas.
+            </p>
+            <Link to="/" className="error-frame__back">
+              Retourner sur la page d’accueil
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </React.Fragment>
   )
 }
 export default Error
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -28,68 +28,68 @@ function Logement() {
   const logRating = parseInt(logX.rating)
   console.log('upupDownStatus: ', { upDownStatus })
   return (
-    <div className="logement">
-      <div className="logement-carousel">
-        <Slideshow pA={logX.pictures} />
-      </div>
-      <div className="logement-section">
-        <div className="logement-section__l1">
-          <div className="logement-section__l1-tit">
-            <p className="logement-section__l1-tit1">{logX.title}</p>
-            <p className="logement-section__l1-tit2">{logX.location}</p>
+    <div>
+      <div className="logement">
+        <div className="logement-carousel">
+          <Slideshow pA={logX.pictures} />
+        </div>
+        <div className="logement-section">
+          <div className="logement-section__l1">
+            <div className="logement-section__l1-tit">
+              <p className="logement-section__l1-tit1">{logX.title}</p>
+              <p className="logement-section__l1-tit2">{logX.location}</p>
+            </div>
+            <div className="logement-section__l1-host">
+              <p className="logement-section__l1-host-name">{logX.host.name}</p>
+              <img
+                src={logX.host.picture}
+                alt="host"
+                className="logement-section__l1-host-img"
+              />
+            </div>
           </div>
-          <div className="logement-section__l1-host">
-            <p className="logement-section__l1-host-name">{logX.host.name}</p>
-            <img
-              src={logX.host.picture}
-              alt="host"
-              className="logement-section__l1-host-img"
+          <div className="logement-section__l2">
+            <div className="logement-section__l2-tags">
+              {tagArr.map((tag, index) => (
+                <div key={index}>
+                  <LogTags tg={tag} />
+                </div>
+              ))}
+            </div>
+            <div className="logement-section__l2-rate">
+              <LogRating starNum={0} lr={logRating} />
+              <LogRating starNum={1} lr={logRating} />
+              <LogRating starNum={2} lr={logRating} />
+              <LogRating starNum={3} lr={logRating} />
+              <LogRating starNum={4} lr={logRating} />
+            </div>
+          </div>
+          <div className="logement-section__l3">
+            <Description
+              desc={logX.description}
+              descArr={descArrowDown}
+              sdescArr={setdescArrow}
+              equiArr={equiArrowDown}
+              uDS={upDownStatus}
+              setuDS={setupDownStatus}
+              uDA={upDownAction}
+              setuDA={setupDownAction}
             />
+            <Equipments
+              equi={logX.equipments}
+              equiArr={equiArrowDown}
+              sequiArr={setequiArrow}
+              descArr={descArrowDown}
+              uDS={upDownStatus}
+              setuDS={setupDownStatus}
+              uDA={upDownAction}
+              setuDA={setupDownAction}
+            />
+            /
           </div>
-        </div>
-        <div className="logement-section__l2">
-          <div className="logement-section__l2-tags">
-            {tagArr.map((tag, index) => (
-              <div key={index}>
-                <LogTags tg={tag} />
-              </div>
-            ))}
-          </div>
-          <div className="logement-section__l2-rate">
-            <LogRating starNum={0} lr={logRating} />
-            <LogRating starNum={1} lr={logRating} />
-            <LogRating starNum={2} lr={logRating} />
-            <LogRating starNum={3} lr={logRating} />
-            <LogRating starNum={4} lr={logRating} />
-          </div>
-        </div>
-        <div className="logement-section__l3">
-          <Description
-            desc={logX.description}
-            descArr={descArrowDown}
-            sdescArr={setdescArrow}
-            equiArr={equiArrowDown}
-            uDS={upDownStatus}
-            setuDS={setupDownStatus}
-            uDA={upDownAction}
-            setuDA={setupDownAction}
-          />
-          <Equipments
-            equi={logX.equipments}
-            equiArr={equiArrowDown}
-            sequiArr={setequiArrow}
-            descArr={descArrowDown}
-            uDS={upDownStatus}
-            setuDS={setupDownStatus}
-            uDA={upDownAction}
-            setuDA={setupDownAction}
-          />
-          /
         </div>
       </div>
-      <div className="logement-footer">
-        <Footer />
-      </div>
+      <Footer />
     </div>
   )
 }
