d4b76dc2b9c51449ee24ef25429f6d01390d6af1
Berkpigh <pierre.berkovits@netplus.ch>
2023-11-06 08:11:53 +0100

    error+++

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Footer from '../../components/Footer'
 
 function Error() {
   return (
     <React.Fragment>
-      <div>
-        <div className="error">
-          <div className="error-frame">
-            <p className="error-frame__p1">404</p>
-            <p className="error-frame__p2">
-              Oups! La page que vous demandez n'existe pas.
-            </p>
-            <Link to="/" className="error-frame__back">
+      <div className="error">
+        <div className="error-frame">
+          <p className="error-frame__p1">404</p>
+          <p className="error-frame__p2-d">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <p className="error-frame__p2-m">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link to="/" className="error-frame__back">
+            <p className="error-frame__back-p">
               Retourner sur la page d’accueil
-            </Link>
-          </div>
+            </p>
+          </Link>
         </div>
       </div>
-      <Footer />
     </React.Fragment>
   )
 }
